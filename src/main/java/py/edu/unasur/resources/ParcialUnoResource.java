7ec2eee54819473472564155356263794c422201
mauriraul14 <mauriraulqui12@gmail.com>
2024-09-17 20:29:21 -0400

    Primeros ejercicios del parcial

---
+++ src/main/java/py/edu/unasur/resources/ParcialUnoResource.java
@@ -0,0 +1,69 @@
+package py.edu.unasur.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/api")
+public class ParcialUnoResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hola desde LP2";
+    }
+
+    //Generar numeros primos
+
+
+    @GET
+    @Path("/prime-numbers/{n}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getPrimeNumbers(@PathParam("n") int n) {
+        List<Integer> primes = new ArrayList<>();
+        int count = 0;
+        int number = 2;
+        while (count < n) {
+            if (isPrime(number)) {
+                primes.add(number);
+                count++;
+            }
+            number++;
+        }
+        return Response.ok( primes).build();
+    }
+
+    private boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
+    //Suma de Digitos
+
+    @GET
+    @Path("/sum-digits/{number}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response sumDigits(@PathParam("number") int number) {
+        int sum = 0;
+        int num = Math.abs(number);
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return Response.ok( sum).build();
+    }
+
+
+    //Secuencia Fibonacci
+
+    
+}
